55791e54b721520a57eb266023e1e5e8a1288df0
Danny Van den Wouwer <dvdwouwe@peopleware.be>
2024-04-10 17:45:28 +0200

    Fix warning

--- src/PPWCode.Server.Core.NServiceBus/MessageContextAccessor.cs
+++ src/PPWCode.Server.Core.NServiceBus/MessageContextAccessor.cs
@@ -1,3 +1,14 @@
+// Copyright 2024 by PeopleWare n.v..
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 using System.Threading;
 using JetBrains.Annotations;
 
@@ -7,15 +18,15 @@ namespace PPWCode.Server.Core.NServiceBus
     [UsedImplicitly]
     public class MessageContextAccessor : IMessageContextAccessor
     {
-        private static readonly AsyncLocal<MessageContextHolder> RequestMessageContextCurrent =
+        private static readonly AsyncLocal<MessageContextHolder> _requestMessageContextCurrent =
             new AsyncLocal<MessageContextHolder>();
 
         public RequestMessageContext MessageContext
         {
-            get => RequestMessageContextCurrent.Value?.Context;
+            get => _requestMessageContextCurrent.Value?.Context;
             set
             {
-                MessageContextHolder holder = RequestMessageContextCurrent.Value;
+                MessageContextHolder holder = _requestMessageContextCurrent.Value;
                 if (holder != null)
                 {
                     // Clear current HttpContext trapped in the AsyncLocals, as its done.
@@ -26,7 +37,7 @@ namespace PPWCode.Server.Core.NServiceBus
                 {
                     // Use an object indirection to hold the HttpContext in the AsyncLocal,
                     // so it can be cleared in all ExecutionContexts when its cleared.
-                    RequestMessageContextCurrent.Value = new MessageContextHolder { Context = value };
+                    _requestMessageContextCurrent.Value = new MessageContextHolder { Context = value };
                 }
             }
         }
